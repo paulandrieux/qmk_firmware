54267e37bb81b9ae526500c64b2fa616cb975318
Paul Andrieux <paul@troopers.email>
2019-02-21 09:53:49 +0100

    add ubuntu conf

--- keyboards/ergodox_ez/keymaps/bepo_ubuntu/keymap.c
+++ keyboards/ergodox_ez/keymaps/bepo_ubuntu/keymap.c
@@ -15,25 +15,33 @@
 // A 'transparent' key code (that falls back to the layers below it).
 #define ___ KC_TRANSPARENT
 
-
-#define CMD MOD_LGUI // rename mac cmd key
-
 #define M_RSFT    MT(MOD_RSFT, BP_M)    // 'M' key and right SHIFT modifier.
-#define W_RCTL    MT(MOD_RCTL, BP_W)    // 'W' key and right control modifier.
+#define W_RCTL    MT(KC_LGUI, BP_W)    // 'W' key and right control modifier.
 #define SPC_RALT  MT(MOD_RALT, KC_SPC)  // SPACE key and right alt modifier.
-#define ENT_CMD  MT(CMD, KC_ENT)  // enter key and cmd modifier.
+#define ENT_CMD  MT(MOD_RCTL, KC_ENT)  // enter key and cmd modifier.
 #define ENT_SWAP  MT(MO(SWAP), KC_ENT)  // enter key and cmd modifier.
-#define SPC_CMD  MT(CMD, KC_SPC)  // SPACE key and cmd modifier.
+#define SPC_CMD  MT(MOD_RCTL, KC_SPC)  // SPACE key and cmd modifier.
 
-#define BSPC_CMD  MT(CMD , KC_BSPC)  // BACKSPACE key and cmd modifier.
+#define BSPC_CMD  MT(MOD_RCTL , KC_BSPC)  // BACKSPACE key and cmd modifier.
 #define TAB_MEH  MT(MEH_T(KC_NO), KC_TAB)  // TAB key and MEH modifier.
 #define LOCK (KC_Q | QK_LGUI | QK_LCTL) // Lock screen cmd+ctrl+q
 
-#define MK_SAVE   LGUI(BP_S)  // cmd + s
-#define MK_COPY   LGUI(BP_C)  // cmd + c
-#define MK_PASTE  LGUI(BP_V)  // cmd + v
-#define MK_UNDO   LGUI(BP_Z)  // cmd + z
-#define MK_REFRESH   LGUI(BP_R)  // cmd + r
+#define MK_SAVE   MT(MO(NUMS), LCTL(BP_S))  // cmd + s
+#define MK_COPY   MT(MO(MOUSE), LCTL(BP_C))  // cmd + c
+#define MK_CUT   LCTL(BP_X)  // cmd + c
+#define MK_PASTE  LCTL(BP_V)  // cmd + v
+#define MK_UNDO   LCTL(BP_Z)  // cmd + z
+#define MK_REFRESH   LCTL(BP_R)  // cmd + r
+
+enum {
+  TD_COPY_CUT = 0
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  //Tap once for Esc, twice for Caps Lock
+  [TD_COPY_CUT]  = ACTION_TAP_DANCE_DOUBLE(MK_COPY, MK_CUT)
+// Other declarations would go here, separated by commas, if you have them
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Layer 0: basic keys.
@@ -42,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,   BP_AMPERSAND, BP_DQOT, BP_SLASH, BP_BACKSLASH, KC_UNDERSCORE, KC_LEFT,
     KC_TAB,   BP_B,    BP_ECUT, BP_P,    BP_O,    BP_EGRV, KC_ESC,
     KC_LSFT,  BP_A,    BP_U,    BP_I,    BP_E,    BP_COMM,
-    KC_LCTRL, BP_AGRV, BP_Y,    BP_X,    BP_DOT,  BP_K,    ALL_T(KC_NO),
-    MK_UNDO,   MK_REFRESH, MK_SAVE, MK_PASTE, MK_COPY,
+    KC_LGUI, BP_AGRV, BP_Y,    BP_X,    BP_DOT,  BP_K,    ALL_T(KC_NO),
+    MK_UNDO,   MK_REFRESH, MK_SAVE, MK_PASTE, MT(MO(FN), TD(TD_COPY_CUT)),
                                                                 KC_LALT, KC_MNXT,
                                                                          KC_MPRV,
                                                   SPC_CMD, ENT_SWAP, KC_MPLY,
@@ -105,9 +113,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [NUMS] = KEYMAP(
     /* left hand */
     ___, ___, ___,    ___,     ___,      ___, ___,
-    ___,     ___,    ___,        ___,     ___,      ___, ___,
-    ___,     ___,    ___,        ___,     ___,      ___,
-    ___,     ___,    ___,     ___, ___, ___, ___,
+    ___,     KC_F1,    KC_F2,        KC_F3,     KC_F4,      KC_F5, ___,
+    ___,     KC_F6,    KC_F7,        KC_F8,     KC_F9,      KC_F10,
+    ___,     KC_F11,    KC_F12,     ___, ___, ___, ___,
     ___,     ___,    ___,        ___,     ___,
                                               ___, ___,
                                                    ___,
